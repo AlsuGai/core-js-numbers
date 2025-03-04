b07d654c9dbabc23eed35e244e93769f41e688e1
AlsuGai <alsu.gai05@mail.ru>
2025-03-04 12:24:49 +0300

    feat: add getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -259,8 +259,16 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  let a = 0;
+  let b = 1;
+  let c = index;
+  for (let i = 2; i <= index; i += 1) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
 }
 
 /**
